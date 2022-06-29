5ecba7e260f82221dc92451256606c15b054340e
Firose Hussain <github@firumon.com>
2022-06-29 12:40:00 +0530

    Service provided added

---
+++ src/EleoiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Milestone\Eleoi;
+
+use Illuminate\Support\ServiceProvider;
+
+class EleoiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
