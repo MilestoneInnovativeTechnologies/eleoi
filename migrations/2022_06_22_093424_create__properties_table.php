d7b8dc443210006b5bf9658e3a7e549a59d1d141
Firose Hussain <github@firumon.com>
2022-07-02 10:33:51 +0530

    Table structure modified

--- migrations/2022_06_22_093424_create__properties_table.php
+++ migrations/2022_06_22_093424_create__properties_table.php
@@ -19,10 +19,10 @@ return new class extends Migration
             $table->string('name',128)->index();
             $table->unsignedBigInteger('value_master')->index()->nullable();
             $table->enum('index',['Y','N'])->index()->default('N');
-            $table->enum('nature',['Single','Multiple','Y/N'])->nullable();
-            $table->enum('control',['text','number','textarea','readonly','hidden','select','radio','checkbox'])->nullable();
+            $table->enum('nature',['Single','Multiple','Y/N'])->nullable()->default('Single');
+            $table->enum('control',['text','number','textarea','readonly','hidden','select','radio','checkbox'])->nullable()->default('text');
             $table->text('auto_on')->nullable();
-            $table->string('auto_formula',255)->nullable();
+            $table->text('auto_formula')->nullable();
             $table->timestamps();
         });
     }
